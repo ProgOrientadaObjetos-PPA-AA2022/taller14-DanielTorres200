7371ff6c88a780df413d0b646be337daaa595b1e
DanielTorres200 <spartanmark136@gmail.com>
2022-07-23 00:37:04 -0500

    Taller 14

--- problema02/Estudiantes/src/estudiantes/Ejecutor.java
+++ problema02/Estudiantes/src/estudiantes/Ejecutor.java
@@ -12,8 +12,13 @@ public class Ejecutor {
 
     public static void main(String[] args) {
         Enlace c = new Enlace();
+        double suma;
         for (int i = 0; i < c.obtenerDataEstudiante().size(); i++) {
-            System.out.printf("%s\n", c.obtenerDataEstudiante().get(i));
+            suma = 0;
+            suma = c.obtenerDataEstudiante().get(i).obtenerCalificacion1()+c.obtenerDataEstudiante().get(i).obtenerCalificacion2()+ c.obtenerDataEstudiante().get(i).obtenerCalificacion3();
+            suma = suma / 3;
+            System.out.printf("%s", c.obtenerDataEstudiante().get(i));
+            System.out.printf("%.2f\n", suma);
         }
     }
 }
